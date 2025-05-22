748704d6a8b11f4ab95dcc670b50e942d3a0bc31
motorych88 <motorych88@mail.ru>
2025-05-22 14:28:42 +0300

    ветка для запуска тестов в дженкинс

--- core/BaseTests.py
+++ core/BaseTests.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--lang=ru")
+    driver = webdriver.Remote(command_executor="http://109.63.137.212:4444", options=options)
     yield driver
-    driver.quit()
+    if driver:
+        driver.quit()
+
